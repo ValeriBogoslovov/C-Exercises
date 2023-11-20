41bb8547331064519f775ff645f99cb3eab0696d
Valeri Bogoslovov <valeribogoslovov@hotmail.com>
2023-11-20 09:03:37 +0200

    Add files via upload

---
+++ absolute_sum/absolute_sum.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int getAbsSum(std::vector<int> arr) {
+	int sum = 0;
+
+	for(auto num : arr) {
+    sum += num;
+	}
+
+	return (sum < 0) ? sum = -sum : sum;
+}
+
+int main() {
+
+  int arrSum = getAbsSum({3, 2, 5, 6});
+
+  cout << arrSum << endl;
+}
