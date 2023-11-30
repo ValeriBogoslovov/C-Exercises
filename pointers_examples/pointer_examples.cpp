b5ab9bf7cb655d2a85c0cbe150a70e6b9913b7b2
Valeri Bogoslovov <valeribogoslovov@hotmail.com>
2023-11-30 15:54:52 +0200

    Array Function
    added function for finding minimum and maximum value of int array using pointers

--- pointers_examples/pointer_examples.cpp
+++ pointers_examples/pointer_examples.cpp
@@ -4,13 +4,17 @@ using namespace std;
 
 // testing some pointer examples
 
+/*
+
 // ***************************** VOID POINTER FUNCTION ***************************************
 // this function will print the value of any type of variable that we pass
 // first parameter is the pointer variable and the second parameter is so we know what we are passing
 // the 'char char_of_variable' parameter can indicate the type of variable we are passing, for example
 // 'i' will be for int, 'c' will be for char, 'd' will be for double etc. this is user choice
 
-/*void print_ptr (void* ptr, char char_of_variable) {
+
+
+void print_ptr (void* ptr, char char_of_variable) {
   // we have to check the char_of_variable and then handle the pointer variable
   if (char_of_variable == 'i') {
     // when we see that the character that we passed is 'i' we know that we have an integer
@@ -22,15 +26,68 @@ using namespace std;
   } else if (char_of_variable == 'c') {
     // we do the same for each type of variable we pass
   }
-}*/
+}
 
-// remove /* */ to use function
+*/ // REMOVE /* */ TO CHECK CODE
 
 // *******************************************************************************************
 
+/*
+
+// ********************* GET MIN MAX VALUE OF ARRAY USING POINTERS FUNCTION ******************
+
+// creating a void function that has four parameter and expects the following
+// array of integers, size of the array, minimum value and maximum value
+// min_value and max_value are passed using pointer, that means we have a reference to them
+// whatever value we change to them will be saved to the original variables
+
+void getMinMaxValue (int num_arr[], int size_array, int* min_value, int* max_value) {
+  // creating a for loop to iterate through the array and compare values and assigning them to min_value and max_value
+
+  for (int i = 1; i < size_array; i++) {
+    // checking if the current element of the array is smaller than the minimum value we passed
+    if (num_arr[i] < *min_value) {
+      *min_value = num_arr[i];
+    } else if (num_arr[i] > *max_value) {
+      *max_value = num_arr[i];
+    }
+  }
+}
+
+//**********************************************************************************************
+*/ // REMOVE /* */ TO CHECK CODE
+
 int main() {
+  /*
+
+  // ********************* GET MIN MAX VALUE OF ARRAY USING POINTERS ***************************
+
+  // initializing integer array and assigning values to it
+  int numbers_arr[] = {4, 12, -333, 532, 28, 83, 1, 0};
+
+  // saving the value of first element of the array as both minimum and maximum value
+  int min_value = numbers_arr[0];
+  int max_value = numbers_arr[0];
+
+  // saving the length (size) of the array
+  // the formula for getting length (size) of array is: (size = sizeof(array) / sizeof(type)) type in this case is int
+  int size_of_arr = sizeof(numbers_arr) / sizeof(int);
+  //cout << size_of_arr << endl; // check size of the array
+
+  // calling the function and passing the parameters
+  // note that we have to pass the address of min_value and max_value (their reference) using '&'
+  getMinMaxValue(numbers_arr, size_of_arr, &min_value, &max_value);
+
+  // printing result
+  cout << "Minimum value of the array of numbers is: " << min_value << endl;
+  cout << "Maximum value of the array of numbers is: " << max_value << endl;
+
+  */ // REMOVE /* */ TO CHECK CODE
+
+  // ******************************************************************************************
 
   /*
+  // ***************************** BASIC POINTER EXAMPLE **************************************
 
   int number = 5; // initializing variable with value of 5
   cout << "Variable value is: " << number << endl; //printing the value
@@ -55,15 +112,18 @@ int main() {
   cout << "New value of variable is: " << number << endl;
   cout << "Address of variable is: " << &number << endl; // Address is the same
 
-  */
-
-  // *************************** // remove /* */ to run code *******************************************************
-
-  // ************************************ VOID POINTER *************************************************************
-
-  //int number = 22;
-  //print_ptr(&number, 'i');
+  */ // REMOVE /* */ TO CHECK CODE
 
   // ***************************************************************************************************************
+
+/*)
+  // ************************************ VOID POINTER *************************************************************
+
+  int number = 22;
+  print_ptr(&number, 'i');
+
+  // ***************************************************************************************************************
+*/ // REMOVE /* */ TO CHECK CODE
+
   return 0;
 }
